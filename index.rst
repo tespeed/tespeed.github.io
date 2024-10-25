bd231e0a53523ff8aca8d36039136639a760a321
rajwaaz <muhammadrajwaazharulalim@gmail.com>
2024-10-25 23:11:05 +0700

    Files Update

--- index.rst
+++ index.rst
@@ -1,9 +1,10 @@
-# Play Tespeed online
-
+================
+Play Tespeed online
+================
 This is a simple HTML & javascript game website where you can play Tespeed online against the computer, an old but challenging board game on an 8×8 checkers-board.
 
-## How to play Tespeed?
-
+How to play Tespeed?
+=======
 The rules are simple:
 
 - The pieces must always move one square at a time, on the black squares.
@@ -13,4 +14,4 @@ The rules are simple:
 - When a piece reaches the last opposing line of the board, it becomes King and can take backward.
 - To win the game, you have to eat all your opponent's pieces or block them so they can not make any valid move.
 
-You can play Tespeed online on https://tespeed.github.io/... Have fun and tell us what you think..
+You can play Tespeed online on https://tespeed.github.io/ Have fun and tell us what you think.
---
+++ lang-en-us.js
@@ -0,0 +1,77 @@
+// English - United States
+
+var lang_desc = "English (US)";
+
+// Elements of the pages
+
+var header = "Play Tespeed Online";
+var game_blurb = "Your ennemy won, want tou replay?";
+var play_game = "Play Checkers";
+var play_again = "Play again!";
+var rules = "Rules %26 Help";
+var reset_scores = "Reset Scores";
+var level_label = "Level";
+var language_label = "Language";
+var undo_move = "Undo";
+var show_about = "About";
+var show_game_log = "Game Log";
+var show_last_move = "Show Last Move";
+var level_hard = "Hard";
+var level_easy = "Easy";
+var status_clean = "Clean";
+var status_cheat = "Cheat";
+
+// MsgArea messages
+
+var game_over_msg = "Game over!";
+var game_over_no_more_pieces_plyr = "Sorry you lost! Maybe next time. Want to Play Again?";
+var game_over_no_more_pieces_comp = "Congratulations.. You won!";
+var game_over_no_more_moves_plyr = "You have lost, you can not make any moves!";
+var game_over_no_more_moves_comp = "You won! Your opponent can not make any moves!";
+var game_over_referee_plyr = "The referee has determined that you are the winner.";
+var game_over_referee_comp = "The referee has determined that you lost.";
+var game_over_referee_drawn = "The referee has determined that this game has been a draw.";
+
+var your_turn = "";
+var wait_for_comp_move = "";
+var comp_move = "";
+var move_undone = "";
+
+var select_a_piece = "";
+var select_own_piece = "";
+var select_destination = "";
+
+var move_not_allowed = "";
+var have_to_take_piece = "You have to take your opponent's piece!";
+var complete_your_move = "";
+var must_complete_your_move = "You have to complete your move.";
+
+var not_your_turn = "";
+var game_already_over = "The game is already over!";
+var player_resigned = "";
+
+// Popup messages
+
+// var referee_warning = "The last 20 moves (10 per player) have only been movements of kings. If no significant change appears within the next 20 moves (10 per player) the referee will decide the winner.";
+var referee_draw = "The referee has determined that this game is a draw.";
+var referee_won_comp = "The referee has determined that this game has been won by the computer.";
+var referee_won_plyr = "The referee has determined that this game has been won by you.";
+var confirm_resign = "Are you sure you want to resign this game?";
+var confirm_resetscores = "Are you sure you want to reset the score?";
+var no_moves_made_yet = "No moves made yet!";
+var nothing_to_undo = "You have made no moves yet!";
+var confirm_firstundo = "Are you sure you want to cheat?";
+var no_button = "No";
+var yes_button = "Yes";
+var ok_button = "OK";
+
+// General
+
+var contributor = "";
+var contributor_site = "";
+var contributor_url = "";
+var loaded_lang_js = 1;
+
+setTimeout('updatePageElements()',100);
+
+// TIAF
